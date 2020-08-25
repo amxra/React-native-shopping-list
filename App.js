d16d82aba3d8735b85adb4714d33e18eadf5b2c4
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-08-25 18:51:42 +0100

    example for stylesheet

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 
 function App (){
   return(
-    <View style ={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
-        <Text style = {{color: 'darkslateblue', fontSize: 50}}>Hello World</Text>
+    <View style ={styles.container}>
+        <Text style = {styles.text}>Hello World</Text>
     </View>
-  )
-}
+  );
+};
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems:'center'
+  },
+  text:{
+    color: 'darkslateblue', 
+    fontSize: 50
+  }
+})
 export default App;
