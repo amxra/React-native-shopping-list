45c993ede862bd92237133efbf603ec3da696a90
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-08-25 20:35:25 +0100

    use useState hook to create items in shopping list

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Header from './components/Header';
+import {v4 as uuid } from 'uuid';
 
 
 function App (){
+  const [items,setItems] =  useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ])
+
   return(
     <View style ={styles.container}>
         <Header />
     </View>
   );
 };
+
+2364564576
 
 const styles = StyleSheet.create({
   container:{
